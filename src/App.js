7463115fbc3ec18ca2bac1ace103c2db75c38067
felipebaloneker <felipebaloneker23@gmail.com>
2021-09-17 16:19:01 -0300

    updated switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import fire from './firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default () =>{
+  const [user,setUser] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [emailError,setEmailError] = useState('');
+  const [passwordError,setPasswordError] = useState('');
+  const [hasAccount,setHasAccount] = useState(false)
+  
+  const handleLogin = () =>{
+    fire
+    .auth()
+    .singInWithEmailAndPassword(email, password)
+    .catch((err)=>{
+      switch(err.code){
+        case "auth/invalid-email":
+        case "auth/user-disabled":
+        case "auth/user-not-found":
+          setEmailError(err.message);
+          break;
+        case "auth/wrong-password":
+          setPasswordError(err.message);
+          break;
+      }
+    });
+  };
+
+  const handleSingup= () =>{
+    fire
+    .auth()
+    .singInWithEmailAndPassword(email, password)
+    .catch((err)=>{
+      switch(err.code){
+        case "auth/email-already-in-use":
+        case "auth/invalid-email":
+          setEmailError(err.message);
+          break;
+        case "auth/weak-password":
+          setPasswordError(err.message);
+          break;
+      }
+    })
+  }
+
+
+  return(
+    <>
+      Hello World!!
+    </>
   );
 }
-
-export default App;
---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5M76Y_R3dsIMv2fBbLdBmZCr3cNTHdwI",
+  authDomain: "telalogin-9ca38.firebaseapp.com",
+  databaseURL: "https://telalogin-9ca38-default-rtdb.firebaseio.com",
+  projectId: "telalogin-9ca38",
+  storageBucket: "telalogin-9ca38.appspot.com",
+  messagingSenderId: "996520196097",
+  appId: "1:996520196097:web:19a55819f39e15508da800"
+};
+
+
+const fire = firebase.initializeApp(firebaseConfig);
